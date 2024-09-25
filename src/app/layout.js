59dbcf6346fba38eb68d8e9bf6144d7da9566630
topdeveloper917 <jamessmith0917topdev@gmail.com>
2024-09-25 17:27:17 -0400

    change icon and add recaptcha

--- src/app/layout.js
+++ src/app/layout.js
@@ -25,7 +25,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
-      <title>Move Mint</title>
+      <title>Movemint</title>
       <body
         className={cn(
           "min-h-screen font-quicksand antialiased",
