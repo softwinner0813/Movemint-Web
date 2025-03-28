d88d2f57866c27a896f3c53dd763f11f2aa090c1
softwinner813 <softwinner813@gmail.com>
2025-03-28 06:56:08 -0700

    fix issue

--- src/components/public/pages/aup/index.js
+++ src/components/public/pages/aup/index.js
@@ -7,64 +7,64 @@ const AcceptUsePolicy = () => {
                 MOVEMINT APP INC. Acceptable Use Policy
             </h1>
 
-            <p className="mb-4">
+            <p className="mb-4">{`
                 This acceptable use policy covers the products, services, and technologies (collectively
                 referred to as the "Products") provided by MOVEMINT APP INC. under any ongoing agreement.
                 It&#39;s designed to protect us, our customers and the general Internet community from unethical,
                 irresponsible and illegal activity.
-            </p>
+            `}</p>
 
-            <p className="mb-4">
+            <p className="mb-4">{`
                 MOVEMINT APP INC. customers found engaging in activities prohibited by this acceptable use
                 policy can be liable for service suspension and account termination. In extreme cases, we
                 may be legally obliged to report such customers to the relevant authorities.
-            </p>
+            `}</p>
 
-            <p className="mb-4">
+            <p className="mb-4">{`
                 This policy was last reviewed on September 11, 2024.
-            </p>
+            `}</p>
 
             <h2 className="text-2xl font-semibold mt-6 mb-2">
                 Fair use
             </h2>
 
-            <p className="mb-4">
+            <p className="mb-4">{`
                 We provide our facilities with the assumption your use will be "business as usual", as per our
                 offer schedule. If your use is considered to be excessive, then additional fees may be charged
                 or capacity may be restricted.
-            </p>
+            `}</p>
 
-            <p className="mb-4">
+            <p className="mb-4">{`
                 We are opposed to all forms of abuse, discrimination, rights infringement and/or any action
                 that harms or disadvantages any group, individual or resource. We expect our customers and,
                 where applicable, their users ("end-users") to likewise engage our Products with similar intent.
-            </p>
+            `}</p>
 
             <h2 className="text-2xl font-semibold mt-6 mb-2">
                 Customer accountability
             </h2>
 
-            <p className="mb-4">
+            <p className="mb-4">{`
                 We regard our customers as being responsible for their own actions as well as for the actions
                 of anyone using our Products with the customer&#39;s permission. This responsibility also applies
                 to anyone using our Products on an unauthorized basis as a result of the customer&#39;s failure to
                 put in place reasonable security measures.
-            </p>
+            `}</p>
 
-            <p className="mb-4">
+            <p className="mb-4">{`
                 By accepting Products from us, our customers agree to ensure adherence to this policy on
                 behalf of anyone using the Products as their end users. Complaints regarding the actions of
                 customers or their end-users will be forwarded to the nominated contact for the account in
                 question.
-            </p>
+            `}</p>
 
-            <p className="mb-4">
+            <p className="mb-4">{`
                 If a customer — or their end-user or anyone using our Products as a result of the customer —
                 violates our acceptable use policy, we reserve the right to terminate any Products associated
                 with the offending account or the account itself or take any remedial or preventative action
                 we deem appropriate without notice. To the extent permitted by law, no credit will be
                 available for interruptions of service resulting from any violation of our acceptable use policy.
-            </p>
+            `}</p>
 
             <h2 className="text-2xl font-semibold mt-6 mb-2">
                 Prohibited activity
@@ -74,10 +74,10 @@ const AcceptUsePolicy = () => {
                 Copyright infringement and access to unauthorized material
             </h3>
 
-            <p className="mb-4">
+            <p className="mb-4">{`
                 Our Products must not be used to transmit, distribute or store any material in violation of any
                 applicable law. This includes but isn&#39;t limited to:
-            </p>
+            `}</p>
 
             <ul className="list-disc list-inside mb-4">
                 <li>
@@ -90,24 +90,24 @@ const AcceptUsePolicy = () => {
                 </li>
             </ul>
 
-            <p className="mb-4">
+            <p className="mb-4">{`
                 The customer is solely responsible for all material they input, upload, disseminate, transmit,
                 create or publish through or on our Products, and for obtaining legal permission to use any
                 works included in such material.
-            </p>
+            `}</p>
 
             <h3 className="text-xl font-semibold mt-4 mb-2">
                 SPAM and unauthorized message activity
             </h3>
 
-            <p className="mb-4">
+            <p className="mb-4">{`
                 Our Products must not be used for the purpose of sending unsolicited bulk or commercial
                 messages in violation of the laws and regulations applicable to your jurisdiction (&#34;spam&#34;). This
                 includes but isn&#39;t limited to sending spam, soliciting customers from spam sent from other
                 service providers, and collecting replies to spam sent from other service providers.
-            </p>
+            `}</p>
 
-            <p className="mb-4">
+            <p className="mb-4">{`
                 Our Products must not be used for the purpose of running unconfirmed mailing lists or
                 telephone number lists ("messaging lists"). This includes but isn&#39;t limited to subscribing email
                 addresses or telephone numbers to any messaging list without the permission of the email
@@ -115,54 +115,54 @@ const AcceptUsePolicy = () => {
                 subscribed in this way. All messaging lists run on or hosted by our Products must be
                 "confirmed opt-in". Verification of the address or telephone number owner&#39;s express
                 permission must be available for the lifespan of the messaging list.
-            </p>
+            `}</p>
 
-            <p className="mb-4">
+            <p className="mb-4">{`
                 We prohibit the use of email lists, telephone number lists or databases purchased from third
                 parties intended for spam or unconfirmed messaging list purposes on our Products.
-            </p>
+            `}</p>
 
-            <p className="mb-4">
+            <p className="mb-4">{`
                 This spam and unauthorized message activity policy applies to messages sent using our
                 Products, or to messages sent from any network by the customer or any person on the
                 customer&#39;s behalf, that directly or indirectly refer the recipient to a site hosted via our
                 Products.
-            </p>
+            `}</p>
 
             <h2 className="text-2xl font-semibold mt-6 mb-2">
                 Unethical, exploitative, and malicious activity
             </h2>
 
-            <p className="mb-4">
+            <p className="mb-4">{`
                 Our Products must not be used for the purpose of advertising, transmitting or otherwise
                 making available any software, program, product or service designed to violate this
                 acceptable use policy, or the acceptable use policy of other service providers. This includes
                 but isn&#39;t limited to facilitating the means to send spam and the initiation of network sniffing,
                 pinging, packet spoofing, flooding, mail-bombing and denial-of-service attacks.
-            </p>
+            `}</p>
 
-            <p className="mb-4">
+            <p className="mb-4">{`
                 Our Products must not be used to access any account or electronic resource where the group
                 or individual attempting to gain access does not own or is not authorized to access the
                 resource (e.g. "hacking", "cracking", "phreaking", etc.).
-            </p>
+            `}</p>
 
-            <p className="mb-4">
+            <p className="mb-4">{`
                 Our Products must not be used for the purpose of intentionally or recklessly introducing
                 viruses or malicious code into our Products and systems.
-            </p>
+            `}</p>
 
-            <p className="mb-4">
+            <p className="mb-4">{`
                 Our Products must not be used for purposely engaging in activities designed to harass
                 another group or individual. Our definition of harassment includes but is not limited to denial-of-service attacks, hate-speech, advocacy of racial or ethnic intolerance, and any activity
                 intended to threaten, abuse, infringe upon the rights of or discriminate against any group or
                 individual.
-            </p>
+            `}</p>
 
 
-            <p className="mb-4">
+            <p className="mb-4">{`
                 Other activities that are considered unethical, exploitative, and malicious include:
-            </p>
+            `}</p>
 
             <ol className="list-disc list-inside mb-4">
                 <li>Obtaining (or attempting to obtain) services from us with the intent to avoid payment;</li>
@@ -176,35 +176,35 @@ const AcceptUsePolicy = () => {
                 <li>Any violation of a person&#39;s privacy.</li>
             </ol>
 
-            <p className="mb-4">
+            <p className="mb-4">{`
                 Our Products may not be used by any person or entity, which is involved with or suspected of
                 involvement in activities or causes relating to illegal gambling; terrorism; narcotics trafficking;
                 arms trafficking or the proliferation, development, design, manufacture, production,
                 stockpiling, or use of nuclear, chemical or biological weapons, weapons of mass destruction,
                 or missiles; in each case including any affiliation with others whatsoever who support the
                 above such activities or causes.
-            </p>
+            `}</p>
 
             <h2 className="text-2xl font-semibold mt-6 mb-2">
                 Unauthorized use of MOVEMINT APP INC. property
             </h2>
 
-            <p className="mb-4">
+            <p className="mb-4">{`
                 We prohibit the impersonation of MOVEMINT APP INC., the representation of a significant
                 business relationship with MOVEMINT APP INC., or ownership of any MOVEMINT APP INC.
                 property (including our Products and brand) for the purpose of fraudulently gaining service,
                 custom, patronage or user trust.
-            </p>
+            `}</p>
 
             <h2 className="text-2xl font-semibold mt-6 mb-2">
                 About this policy
             </h2>
 
-            <p className="mb-4">
+            <p className="mb-4">{`
                 This policy outlines a non-exclusive list of activities and intent we deem unacceptable and
                 incompatible with our brand. We reserve the right to modify this policy at any time by publishing the revised version on our
                 app or website. The revised version will be effective from the earlier of:
-            </p>
+            `}</p>
 
             <ul className="list-disc list-inside mb-4">
                 <li>
